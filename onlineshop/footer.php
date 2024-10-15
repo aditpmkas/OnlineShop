dfb03585a5c2015f9d8f02232fb34f1bff17080e
bisulpetjah <aditya.pamungkas.sdn@gmail.com>
2024-10-16 04:42:48 +0700

    Add icon Facebook footer.php

--- onlineshop/footer.php
+++ onlineshop/footer.php
@@ -1,7 +1,10 @@
 <div class="container-fluid py-5 content-subscribe text-light">
-        <div class="container">
+<div class="container">
+            <h5 class="text-center mb-4">Meet Us</h5>
         <div class="row justify-content-center">
-
+            <div class="col-sm-1 d-flex justify-content-center mb-2">
+                <i class="fab fa-facebook fs-1"></i>
+            </div>
         </div>
       </div>
     </div>
